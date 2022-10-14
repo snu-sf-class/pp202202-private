4eaaea66e4c3287898c44aa12c697eefd77522ed
Ho Young Jhoo <starvessel@naver.com>
2022-10-14 11:07:43 +0900

    fix compile script

--- assignments/hw2/compile.bat
+++ assignments/hw2/compile.bat
@@ -1,4 +1,5 @@
 rd /s /q classes
 mkdir classes
+call scalac -classpath classes/ -d classes/ src/hw2/Data.scala
 call scalac -classpath classes/ -d classes/ src/hw2/Main.scala
 call scalac -classpath classes/ -d classes/ test/hw2/Test.scala
--- assignments/hw2/compile.sh
+++ assignments/hw2/compile.sh
@@ -2,5 +2,6 @@
 
 rm -rf classes
 mkdir classes
+scalac -classpath classes/ -d classes/ src/hw2/Data.scala
 scalac -classpath classes/ -d classes/ src/hw2/Main.scala
 scalac -classpath classes/ -d classes/ test/hw2/Test.scala
