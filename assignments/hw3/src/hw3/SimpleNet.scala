b6871bfbb5738c066cbad1a1a24dbb435974509b
Ho Young Jhoo <starvessel@naver.com>
2022-11-17 20:12:33 +0900

    fix simplenet reshape

--- assignments/hw3/src/hw3/SimpleNet.scala
+++ assignments/hw3/src/hw3/SimpleNet.scala
@@ -31,6 +31,7 @@ import hw3.nnmodule.{Linear, NNModule, ReLU, Sequential, Softmax, SystolicLinear
  *
  * @param weights every weight of the linear
  */
+
 class SimpleNet(val weights: Array[Matrix]) extends NNModule {
   private type LN = Linear
 
@@ -40,17 +41,21 @@ class SimpleNet(val weights: Array[Matrix]) extends NNModule {
   protected val seq2: (NNModule, NNModule) = (Sequential(new LN(weights(4)), ReLU, new LN(weights(5))), new LN(weights(6)))
   protected val seq3: (NNModule, NNModule) = (Sequential(new LN(weights(7)), ReLU, new LN(weights(8))), new LN(weights(9)))
 
-  protected val tail: NNModule = Sequential(new LN(weights(10)), Softmax)
+  protected val tail: NNModule = new LN(weights(10))
 
   def apply(input: NDArray): NDArray = {
-    val flat = if (input.ndim == 3) input.reshape(input.getShape(0), 784) else input.reshape(784)
+    val batch = input.getShape(0)
+    val isBatched = input.ndim >= 3
+    val flat = if (isBatched) input.reshape(input.getShape(0), 784, 1) else input.reshape(784, 1)
 
     val x1 = head(flat)
     val x2 = ReLU(seq1._1(x1).add(seq1._2(x1)))
     val x3 = ReLU(seq2._1(x2).add(seq2._2(x2)))
     val x4 = ReLU(seq3._1(x3).add(seq3._2(x3)))
 
-    tail(x4)
+    val x5 = tail(x4)
+    val x6 = if (isBatched) x5.reshape(batch, 10) else x5.reshape(10)
+    Softmax(x6)
   }
 }
 
@@ -63,6 +68,6 @@ class SystolicNet(weights: Array[Matrix]) extends SimpleNet(weights) {
   override protected val seq2: (NNModule, NNModule) = (Sequential(new LN(weights(4)), ReLU, new LN(weights(5))), new LN(weights(6)))
   override protected val seq3: (NNModule, NNModule) = (Sequential(new LN(weights(7)), ReLU, new LN(weights(8))), new LN(weights(9)))
 
-  override protected val tail: NNModule = Sequential(new LN(weights(10)), Softmax)
+  override protected val tail: NNModule = new LN(weights(10))
 }
 
